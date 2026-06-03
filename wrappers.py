a7c5618c3384051077e05b97db30267ae73c2ac0
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-06-03 10:08:26 -0600

    refactor: remove BraxGymnaxWrapper, NavixGymnaxWrapper

--- wrappers.py
+++ wrappers.py
@@ -4,11 +4,8 @@ import chex
 import numpy as np
 from flax import struct
 from functools import partial
-from typing import Optional, Tuple, Union, Any
+from typing import Optional, Tuple, Union
 from gymnax.environments import environment, spaces
-from brax import envs
-from brax.envs.wrappers.training import EpisodeWrapper, AutoResetWrapper
-import navix as nx
 
 
 class GymnaxWrapper(object):
@@ -112,64 +109,6 @@ class LogWrapper(GymnaxWrapper):
         info["timestep"] = state.timestep
         info["returned_episode"] = done
         return obs, state, reward, done, info
-
-
-class BraxGymnaxWrapper:
-    def __init__(self, env_name, backend="positional"):
-        env = envs.get_environment(env_name=env_name, backend=backend)
-        env = EpisodeWrapper(env, episode_length=1000, action_repeat=1)
-        env = AutoResetWrapper(env)
-        self._env = env
-        self.action_size = env.action_size
-        self.observation_size = (env.observation_size,)
-
-    def reset(self, key, params=None):
-        state = self._env.reset(key)
-        return state.obs, state
-
-    def step(self, key, state, action, params=None):
-        next_state = self._env.step(state, action)
-        return next_state.obs, next_state, next_state.reward, next_state.done > 0.5, {}
-
-    def observation_space(self, params):
-        return spaces.Box(
-            low=-jnp.inf,
-            high=jnp.inf,
-            shape=(self._env.observation_size,),
-        )
-
-    def action_space(self, params):
-        return spaces.Box(
-            low=-1.0,
-            high=1.0,
-            shape=(self._env.action_size,),
-        )
-
-
-class NavixGymnaxWrapper:
-    def __init__(self, env_name):
-        self._env = nx.make(env_name)
-
-    def reset(self, key, params=None):
-        timestep = self._env.reset(key)
-        return timestep.observation, timestep
-
-    def step(self, key, state, action, params=None):
-        timestep = self._env.step(state, action)
-        return timestep.observation, timestep, timestep.reward, timestep.is_done(), {}
-
-    def observation_space(self, params):
-        return spaces.Box(
-            low=self._env.observation_space.minimum,
-            high=self._env.observation_space.maximum,
-            shape=(np.prod(self._env.observation_space.shape),),
-            dtype=self._env.observation_space.dtype,
-        )
-
-    def action_space(self, params):
-        return spaces.Discrete(
-            num_categories=self._env.action_space.maximum.item() + 1,
-        )
 
 
 class ClipAction(GymnaxWrapper):
